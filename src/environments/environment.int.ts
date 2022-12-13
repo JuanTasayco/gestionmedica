bacba28a5f168e660e47f0ad7fed702ed347f746
Ryky Azanero <rickykaiser@gmail.com>
2022-12-12 21:34:29 -0500

    config int y prod

---
+++ src/environments/environment.int.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  urlService: 'https://api-int.pre.mapfreperu.com/legacy/salud/wsGestionMedica/gestionMedica/',
+  urlService_cronicos: 'https://api-int.pre.mapfreperu.com/legacy/salud/wsGestionMedica/',//url para cronicos
+  oimHome: 'https://oim-int.pre.mapfre.com.pe/',
+  oimApiPath: 'https://oim-int.pre.mapfre.com.pe/oim_new_login/',
+  logoutPath: 'api/seguridad/acceso/logout'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  urlService: 'https://api.mapfre.com.pe/legacy/salud/tecuidamos/',
+  urlService: 'https://api.mapfreperu.com/legacy/salud/wsGestionMedica/gestionMedica/',
+  urlService_cronicos: 'https://api.mapfreperu.com/legacy/salud/wsGestionMedica/',//url para cronicos
   oimHome: 'https://oim.mapfre.com.pe/',
   oimApiPath: 'https://oim.mapfre.com.pe/oim_new_login/',
   logoutPath: 'api/seguridad/acceso/logout'
